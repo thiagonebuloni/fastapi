428e47f168f71597ca2a2d6c94313e5cb94426e8
thiagonebuloni <thiago.nebuloni@gmail.com>
2023-07-23 08:03:27 -0300

    deploying to ubuntu via actions

--- app/main.py
+++ app/main.py
@@ -29,4 +29,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello world successfully deployed from CI/CD pipeline!"}
+    return {"message": "Hello world successfully deployed on Ubuntu!"}
